20513ef8f3abfaaf6514b044698866ef14e5bfbe
Eugene Lu <eugenelu9848@gmail.com>
2021-03-05 17:23:25 -0700

    Initial setup

---
+++ piccollage-minesweeper/piccollage-minesweeper/Controller/MainController.swift
@@ -0,0 +1,19 @@
+//
+//  MainController.swift
+//  piccollage-minesweeper
+//
+//  Created by Eugene Lu on 2021-03-05.
+//
+
+import UIKit
+
+class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
